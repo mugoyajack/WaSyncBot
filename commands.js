f380f78896daf52d056c1fbd637a2e0a18e8a947
mugoyajack <mugoyajack3@gmail.com>
2025-08-10 20:33:21 +0300

    Initial Commit

---
+++ commands.js
@@ -0,0 +1,42 @@
+module.exports = {
+    // COMMAND-ALIASES ONLY CHANGE THE SECOND PART OF THE COMMANDS, NOT THE FIRST PART
+    // DO NOT CHANGE THE FIRST PART OF THE COMMANDS!!!!!
+    // DO NOT INCLUDE THE DOT (.) IN THE COMMANDS
+
+    help: "help", //Displays the help message
+    ping: "ping", //Checks if the bot is online
+    license: "license", //Displays the license information
+
+    addmessagetemplate: "amt", //Adds a message template for the broadcasting feature
+    deletemessagetemplate: "dmt", //Deletes a message template for the broadcasting feature
+    getmymessagetemplates: "gmmt", //Gets the message templates for the broadcasting feature
+  
+    getmygroups: "gmg", //Gets the groups you are part of
+    getallmylists: "gaml", //Gets all your lists
+
+    addgrouptobroadcastlist: "agtbl", //Adds a group to the broadcast list
+    removegroupfrombroadcastlist: "rgfbl", //Removes a group from the broadcast list
+    createbroadcastlist: "cbl", //Creates a new broadcast list
+    deletebroadcastlist: "dbl", //Deletes a broadcast list
+    getmybroadcastlists: "gbl", //Gets your broadcast lists
+    addgrouptodonotbroadcastlist: "agtdnbl", //Adds a group to the do not broadcast list
+    rmgroupfromdonotbroadcastlist: "rmgfdnbl", //Removes a group from the do not broadcast list
+
+    removegroupmembersfrommentions: "rgmfm", //Removes group members from the do not mention list
+    addgroupmemberstomentions: "agmfm", //Adds group members to the do not mention list
+
+    resetmygroupid: "rmgi", //Resets the group sender ID
+
+    getgrouplinks: "ggl", //Gets the fetched group links
+
+    broadcastmessage: "bm", //Broadcasts a message to all groups in the broadcast list
+    getbroadcasttemplates: "gbt", //Gets the broadcast templates
+    startbroadcast: "sb", //Starts a broadcast
+    pausebroadcast: "pb", //Pauses a broadcast
+    resumebroadcast: "rb", //Resumes a paused broadcast
+    cancelbroadcast: "cb", //Cancels a broadcast
+    deletebroadcasttemplate: "dbt", //Deletes a message template for the broadcasting feature
+    getbroadcastedgroups: "gbg", //Gets the groups that have been broadcasted to
+  // DO NOT CHANGE THE FIRST PART OF THE COMMANDS!!!!!
+  // CONTACT BOT OWNER FOR HELP IF STUCK
+}
---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "phoneNumber": "254707142232", // Your WhatsApp phone number in international format without the '+' sign
+  "sessionName": "whatsapp-bot-session",
+  "logLevel": "silent",
+  "minPauseBetweenMessages":  30, // Minimum pause between messages in seconds
+  "maxPauseBetweenMessages": 120, // Maximum pause between messages in seconds
+  "saveInBatchMode":true, // Save runtime files in batch mode
+  "saveBatchSize":1000, // Number of changes before writing to disk
+  };
