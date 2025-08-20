440cfbd0808f14f50c099959ec7673970a889c36
mugoyajack <mugoyajack3@gmail.com>
2025-08-20 19:25:38 +0300

    Released version 1.0.1

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ module.exports = {
   "phoneNumber": "", // Your WhatsApp phone number in international format (e.g., "1234567890")
   "sessionName": "whatsapp-bot-session",
   "logLevel": "silent",
-  "minPauseBetweenMessages": 10,
-  "maxPauseBetweenMessages": 30,
+  "minPauseBetweenMessages": 30,
+  "maxPauseBetweenMessages": 60,
   "saveInBatchMode": true,
   "saveBatchSize": 1000,
-  "WARN_UNAUTHORISED": true,
+  "WARN_UNAUTHORISED": false,
   "AUTO_JOIN_GROUPS": true,
   "SHOW_COMMANDS": false, 
 };
